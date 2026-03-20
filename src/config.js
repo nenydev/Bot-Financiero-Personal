1680d9f793711369d679f56add7e444726bc8aa9
Henry Silva <henrysjr2004@gmail.com>
2026-03-20 14:15:51 -0300

    add kiara to whitelist

--- src/config.js
+++ src/config.js
@@ -14,6 +14,8 @@ import 'dotenv/config';
 export const WHITELIST = [
   { id: 5966834377, nombre: 'Henry', email: 'nenydev@gmail.com' },
   { id: 8368683187, nombre: 'Andres', email: 'almirante2004@gmail.com' },
+  { id: 8488722233, nombre: 'Kiara', email: 'beniteskiara2@gmail.com' },
+
 ];
 
 export const config = {
